08876f45f3ad9cf36e085bbdb83dfec0bc234ef5
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-03-02 20:12:27 +0700

    Update Font

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //menghilangkan debug banner
       home: Scaffold(
-        body: Center(child: Text("Hallo ini body")),
+        body: Center(
+          child: Text(
+            "Hallo ini body, gue mau test penempatan text alignnya udah bener atau belum",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'tiltneon',
+              fontSize: 18,
+            ),
+          ),
+        ),
         appBar: AppBar(
           centerTitle: false, //ganti true kalau mau titlenya ditengah
           title: Text("Title AppBar"),

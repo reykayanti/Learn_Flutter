8a25d9fbfe02e7538192dac1daf25fe434af2109
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-03-09 23:29:41 +0700

    Membuat tampilan statefull
    - Membuat tampilan statefull (masih stateles)

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:faker/faker.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  var faker = new Faker();
+  int angkaAdd = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Extract Widget"),
-        ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return ChatItem(
-              imageUrl: "https://picsum.photos/id/$index/200/300",
-              title: faker.person.name(),
-              subtitle: faker.lorem.sentence(),
-            );
-          },
-        ),
-        // body: ListView(
-        //   children: [
-        //     ChatItem(
-        //       imageUrl: "https://picsum.photos/id/1/200/300",
-        //       title: "Dika",
-        //       subtitle: "loremlalallala",
-        //     )
-        //   ],
-        // ),
-      ),
-    );
-  }
-}
-
-class ChatItem extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String subtitle;
-
-  const ChatItem(
-      {super.key,
-      required this.imageUrl,
-      required this.title,
-      required this.subtitle});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.only(top: 5, bottom: 5, left: 3, right: 3),
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(imageUrl),
-      ),
-      title: Text(title),
-      subtitle: Text(subtitle),
-      trailing: Text("10.00 AM"),
+          appBar: AppBar(
+            title: Text("Statefull Angka"),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                angkaAdd.toString(),
+                style: TextStyle(fontSize: 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.remove),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.add),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }

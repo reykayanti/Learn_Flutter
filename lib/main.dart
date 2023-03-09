ec2c4d720820e02dabdbd443c4c505d254ee7de4
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-03-09 23:55:15 +0700

    Refactor to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,13 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  int angkaAdd = 1;
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int angkaAdd = 3;
 
   @override
   Widget build(BuildContext context) {
@@ -18,18 +23,44 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
+                "Min di angka 0 dan Max diangka 10.",
+                style: TextStyle(fontSize: 14),
+              ),
+              Text(
                 angkaAdd.toString(),
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(
+                  fontSize: 20 +
+                      double.parse(
+                        //untuk mengubah nilai angkaAdd menjadi string, lalu dari string menjadi double
+                        angkaAdd.toString(),
+                      ),
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      if (angkaAdd != 0) {
+                        setState(() {
+                          angkaAdd--;
+                        });
+                      }
+                      // print ini hanya untuk muncul di debug console
+                      // print(angkaAdd);
+                    },
                     child: Icon(Icons.remove),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      if (angkaAdd != 10) {
+                        setState(() {
+                          angkaAdd++;
+                        });
+                      }
+                      // print ini hanya untuk muncul di debug console
+                      // print(angkaAdd);
+                    },
                     child: Icon(Icons.add),
                   ),
                 ],

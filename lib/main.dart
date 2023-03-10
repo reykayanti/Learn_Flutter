9ac6c98ed3214fd23873b673563454d829cfe3eb
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-03-10 21:42:43 +0700

    Mapping list

--- lib/main.dart
+++ lib/main.dart
@@ -4,69 +4,79 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int angkaAdd = 3;
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> myList = [
+    {
+      "name": "Eka",
+      "age": 22,
+      "favColor": ["black", "red", "green"],
+    },
+    {
+      "name": "Eka",
+      "age": 22,
+      "favColor": ["black", "red", "green"],
+    },
+    {
+      "name": "Eka",
+      "age": 22,
+      "favColor": [
+        "black",
+        "red",
+        "green",
+        "black",
+        "red",
+        "green",
+        "black",
+        "red",
+        "green"
+      ],
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Statefull Angka"),
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                "Min di angka 0 dan Max diangka 10.",
-                style: TextStyle(fontSize: 14),
-              ),
-              Text(
-                angkaAdd.toString(),
-                style: TextStyle(
-                  fontSize: 20 +
-                      double.parse(
-                        //untuk mengubah nilai angkaAdd menjadi string, lalu dari string menjadi double
-                        angkaAdd.toString(),
-                      ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+        appBar: AppBar(
+          title: Text("Mapping"),
+        ),
+        body: ListView(
+          children: myList.map((data) {
+            // print(data['favColor']);
+            List favColor = data['favColor'];
+            return Card(
+              child: Column(
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (angkaAdd != 0) {
-                        setState(() {
-                          angkaAdd--;
-                        });
-                      }
-                      // print ini hanya untuk muncul di debug console
-                      // print(angkaAdd);
-                    },
-                    child: Icon(Icons.remove),
+                  Padding(padding: EdgeInsets.all(10)),
+                  Row(
+                    children: [
+                      CircleAvatar(),
+                      Column(
+                        children: [
+                          // Text("Name : ${data['name']}"),
+                          Text("Name : " + data['name']),
+                          Text("Age : ${data['age']}"),
+                          // Text("Age : " + data["age"].toString()),
+                        ],
+                      )
+                    ],
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (angkaAdd != 10) {
-                        setState(() {
-                          angkaAdd++;
-                        });
-                      }
-                      // print ini hanya untuk muncul di debug console
-                      // print(angkaAdd);
-                    },
-                    child: Icon(Icons.add),
+                  Row(
+                    children: favColor.map((color) {
+                      return Container(
+                        child: Text(color),
+                        color: Colors.amber,
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.all(10),
+                      );
+                    }).toList(),
                   ),
                 ],
-              )
-            ],
-          )),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }

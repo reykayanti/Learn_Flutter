83dc898032bed588571b2b2f44978fb8cd297a7a
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-05-05 11:28:57 +0700

    update

---
+++ lib/datetime_appbar.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Datetime & Appbar"),
+          backgroundColor: Colors.amber,
+          actions: [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Image(
+                height: 20,
+                width: 20,
+                image: AssetImage('images/search.png'),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Image(
+                height: 20,
+                width: 20,
+                image: AssetImage('images/user.png'),
+              ),
+            )
+          ],
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 240, 240, 240),
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Menampilkan tanggal menggunakan package intl.",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          DateTime.now().toString(),
+                          style: TextStyle(fontSize: 15),
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    height: 180,
+                    width: 380,
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(10),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Menampilkan tanggal menggunakan package intl.",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          DateTime.now().toString(),
+                          style: TextStyle(fontSize: 15),
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    height: 180,
+                    width: 380,
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(10),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,72 +5,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  List<Tab> myTab = [
+    Tab(
+      text: "tab2",
+    ),
+    Tab(
+      text: "tab3",
+    ),
+    Tab(
+      icon: Icon(Icons.login),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Datetime & Appbar"),
-          backgroundColor: Colors.amber,
-          actions: [
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Image(
-                height: 20,
-                width: 20,
-                image: AssetImage('images/search.png'),
-              ),
+          appBar: AppBar(
+        title: Text("My Apps"),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: DefaultTabController(
+            length: myTab.length,
+            child: TabBar(
+              labelColor: Colors.black,
+              indicator: BoxDecoration(color: Colors.pink),
+              tabs: myTab,
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Image(
-                height: 20,
-                width: 20,
-                image: AssetImage('images/user.png'),
-              ),
-            )
-          ],
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 240, 240, 240),
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Menampilkan tanggal menggunakan package intl.",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                        Text(
-                          DateTime.now().toString(),
-                          style: TextStyle(fontSize: 15),
-                        )
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    ),
-                    height: 180,
-                    width: 380,
-                    margin: EdgeInsets.only(top: 20),
-                    padding: EdgeInsets.all(10),
-                  ),
-                ],
-              )
-            ],
           ),
         ),
-      ),
+      )),
     );
   }
 }
